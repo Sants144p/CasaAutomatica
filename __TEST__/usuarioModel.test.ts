b14da51f4d3770862dae61c99a4236ad90ff35f1
LucasPLimal <pereira.lima1@escolar.ifrn.edu.br>
2025-03-03 22:56:21 -0300

    test uni com getall e getbyid

--- __TEST__/usuarioModel.test.ts
+++ __TEST__/usuarioModel.test.ts
@@ -1,12 +1,29 @@
-import { getUsuarioById } from "../src/Model/usuarioModel"; 
+import { getUsuarioById, getAllUsuarios } from "../src/Model/usuarioModel";
 
 jest.mock("../src/Model/usuarioModel", () => ({
-  getUsuarioById: jest.fn()
+  getUsuarioById: jest.fn(),
+  getAllUsuarios: jest.fn()
 }));
 
-test("Deve retornar um usuário do banco", async () => {
-  (getUsuarioById as jest.Mock).mockResolvedValue({ id: 1, nome: "leitoa" });
+describe("Testes do usuarioModel", () => {
+  
+  test("Deve retornar um usuário pelo ID", async () => {
+    (getUsuarioById as jest.Mock).mockResolvedValue({ id: 1, nome: "leitoa" });
 
-  const usuario = await getUsuarioById(1);
-  expect(usuario).toEqual({ id: 1, nome: "leitoa" });
+    const usuario = await getUsuarioById(1);
+    expect(usuario).toEqual({ id: 1, nome: "leitoa" });
+  });
+
+  test("Deve retornar todos os usuários do banco", async () => {
+    const usuariosMock = [
+      { id: 1, nome: "leitoa" },
+      { id: 2, nome: "robertinho" },
+      { id: 3, nome: "leozinho" }];
+
+    (getAllUsuarios as jest.Mock).mockResolvedValue(usuariosMock);
+
+    const usuarios = await getAllUsuarios();
+    expect(usuarios).toEqual(usuariosMock);
+  });
+  
 });

26ec5cdf6961a342060139f16c3fd39d8b66a8fa
LUCAS PEREIRA LIMA <20231144010053@ifrn.local>
2025-03-07 15:57:16 -0300

    ta funcinando porra

--- __TEST__/usuarioModel.test.ts
+++ __TEST__/usuarioModel.test.ts
@@ -1,6 +1,6 @@
 
 import pool from '../src/database';
-import { getAllUsuarios } from '../src/Model/usuarioModel'; // Ajuste o caminho conforme necessário
+import { getAllUsuarios, getUsuarioById } from '../src/Model/usuarioModel'; // Ajuste o caminho conforme necessário
 
 // Mock do pool.execute
 jest.mock('../src/database', () => ({
@@ -38,4 +38,32 @@ describe('getAllUsuarios', () => {
     // Verifica se o pool.execute foi chamado corretamente
     expect(pool.execute).toHaveBeenCalledWith('SELECT * FROM usuarios');
   });
+
+
 });
+
+describe('getUsuarioById', () => {
+  it('deve retornar um usuario por ID', async () => {
+    const mockUsuarios = [{ id: 1, nome: "michael guy", senha: "7minutoz"},
+                          { id: 2, nome: 'Usuário 2', senha: 'senha2' }];
+    (pool.execute as jest.Mock).mockResolvedValueOnce([mockUsuarios]);
+    const usuarios = await getUsuarioById(1);
+
+    // Verifica se o resultado é o esperado
+    expect(usuarios).toEqual(mockUsuarios[0]);
+
+    // Verifica se o pool.execute foi chamado corretamente
+    expect(pool.execute).toHaveBeenCalledWith("SELECT * FROM usuarios WHERE id = ?", [1]);
+  })
+  it('deve lançar um erro se a consulta falhar', async () => {
+    // Configura o mock para lançar um erro
+    (pool.execute as jest.Mock).mockRejectedValueOnce(new Error('Erro ao buscar usuários'));
+
+    // Verifica se a função lança o erro esperado
+    await expect(getUsuarioById).rejects.toThrow('Erro ao buscar usuários');
+
+    // Verifica se o pool.execute foi chamado corretamente
+    expect(pool.execute).toHaveBeenCalledWith("SELECT * FROM usuarios WHERE id = ?", [1]);
+  });
+
+})
